ff5e1c7ba71ded8d1f0bd3f0f62a44d312388fe2
sebacastillo <castilloclaudiosebastian@gmail.com>
2023-07-18 18:29:48 -0300

    update with AnnotatedTransformer_from_scratch

--- src/utils.py
+++ src/utils.py
@@ -9,3 +9,15 @@ def concatenate_text_files(corpus_path, output_file):
     print(f"All text files in {corpus_path} have been concatenated into {output_file}.")
 
 concatenate_text_files('/home/sebacastillo/mineriatexto/data/corpus/borges/', '/home/sebacastillo/mineriatexto/data/borges_combined.txt')
+
+# read all files and append to df
+def concatenate_text_files_to_dataframe(corpus_path):
+    data = []
+    for fname in os.listdir(corpus_path):
+        if fname.endswith('.txt'):
+            fpath = os.path.join(corpus_path, fname)
+            with open(fpath, 'r', encoding='utf-8') as infile:
+                file_content = infile.read()
+                data.append([fname, file_content])
+    df = pd.DataFrame(data, columns=['filename', 'text'])
+    return df
